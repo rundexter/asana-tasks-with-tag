5d454abfa7fee2312a8a2f71e336a1deb8ebe5f7
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-12-02 14:07:44 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,91 @@
+var _ = require('lodash');
+var request = require('request').defaults({
+    baseUrl: 'https://app.asana.com/api/1.0/'
+});
+var globalPickResult = {
+    'data': {
+        key: 'data',
+        fields: {
+            id: 'id',
+            name: 'name'
+        }
+    }
+};
+
 module.exports = {
+    /**
+     * Return pick result.
+     *
+     * @param output
+     * @param pickResult
+     * @returns {*}
+     */
+    pickResult: function (output, pickResult) {
+        var result = {};
+
+        _.map(_.keys(pickResult), function (resultVal) {
+
+            if (_.has(output, resultVal)) {
+
+                if (_.isObject(pickResult[resultVal])) {
+                    if (_.isArray(_.get(output, resultVal))) {
+
+                        if (!_.isArray(result[pickResult[resultVal].key])) {
+                            result[pickResult[resultVal].key] = [];
+                        }
+
+                        _.map(_.get(output, resultVal), function (inOutArrayValue) {
+
+                            result[pickResult[resultVal].key].push(this.pickResult(inOutArrayValue, pickResult[resultVal].fields));
+                        }, this);
+                    } else if (_.isObject(_.get(output, resultVal))){
+
+                        result[pickResult[resultVal].key] = this.pickResult(_.get(output, resultVal), pickResult[resultVal].fields);
+                    }
+                } else {
+                    _.set(result, pickResult[resultVal], _.get(output, resultVal));
+                }
+            }
+        }, this);
+
+        return result;
+    },
+
+    /**
+     * Return auth object.
+     *
+     *
+     * @param dexter
+     * @returns {*}
+     */
+    authParams: function (dexter) {
+        var res = {};
+
+        if (dexter.environment('asana_access_token')) {
+            res = {
+                bearer: dexter.environment('asana_access_token')
+            };
+        } else {
+            this.fail('A [asana_access_token] env variables need for this module');
+        }
+
+        return res;
+    },
+
+    /**
+     * Send api request.
+     *
+     * @param method
+     * @param api
+     * @param options
+     * @param auth
+     * @param callback
+     */
+    apiRequest: function (method, api, options, auth, callback) {
+
+        request[method]({url: api, form: options, auth: auth, json: true}, callback);
+    },
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +93,17 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var auth = this.authParams(dexter);
+
+        this.apiRequest('get', 'tags/' + step.input('tag').first() + '/tasks', {}, auth, function (error, responce, body) {
+
+            if (error || body.errors) {
+
+                this.fail(error || body.errors);
+            } else {
+
+                this.complete(this.pickResult(body, globalPickResult));
+            }
+        }.bind(this));
     }
 };
